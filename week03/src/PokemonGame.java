934956ae83216061fc64706ae3b61566254e7db0
namkyu0127 <2020136112@g.shingu.ac.kr>
2023-09-13 15:51:35 +0900

    week03 v11

--- week03/src/PokemonGame.java
+++ week03/src/PokemonGame.java
@@ -1,19 +1,28 @@
 
-
 public class PokemonGame {
     public static void main(String[] args) {
         Squirtle s1 = new Squirtle();
         Pikachu p1 = new Pikachu();
-        Charizard c1 = new Charizard();
 
-//        s1.setHp(50);
-//        s1.setLevel(1);
-        c1.evolve();
-        System.out.println(c1.getLevel());
-        System.out.println(c1.getHp());
+        s1.evolve();
 
-        p1.attack();
-        c1.attack();
-        s1.attack();
+//        Pokemon p1 = new Pikachu();  // upcast,  부모 클래스 타입의 변수는 자식 클래스 객체를 받을 수 있다
+//        Pikachu p2 = new Pikachu();  // 캐스팅 X (형변환 X)
+//        //Pikachu p3 = new Pokemon();  // downcast 불가, 자식 클래스 타입의 변수는 부모 클래스의 객체를 변환할 수 없다.
+//
+//        System.out.println(p1);
+//        System.out.println(p2);
+//
+//        p1.attack();
+//        p2.attack();
+//
+//        //p1.type(); // Pokemon 변수 (부모 클래스 타입의 변수)로 Pikachu 전용 (자식 클래스 전용) 메서드를 사용할 수 없다
+//        p2.type();  // O
+//
+//        // 명시적 형변환 (명시적 캐스팅)
+//        Charizard p4 = (Charizard)p1;  // 런타임 에러
+//        Pikachu p3 = (Pikachu)p1; // downcast 성공, 힙메모리에 생성된 원본 객체가 Pikachu 이기 때문에 다운캐스팅이 가능.
+//        System.out.println(p3);
+//        p3.type();
     }
 }
