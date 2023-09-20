bd8ac8f67fc51b7efe737b4e3bad35593f89c066
namkyu0127 <2020136112@g.shingu.ac.kr>
2023-09-20 14:40:31 +0900

    week04 v19

--- week04/src/PokemonGame.java
+++ week04/src/PokemonGame.java
@@ -1,8 +1,11 @@
 
 public class PokemonGame {
     public static void main(String[] args) {
-        Pikachu pikachu = new Pikachu();
-        pikachu.setFlyable(new NoFly()); // 프로그램 실행 중 동적으로 나는 방식을 변경
+        NoFly noFly = new NoFly();
+        Pikachu pikachu = new Pikachu(noFly);
+       // pikachu.setFlyable(new NoFly()); // 프로그램 실행 중 동적으로 나는 방식을 변경
+
+
         pikachu.performFly();
     }
 }
