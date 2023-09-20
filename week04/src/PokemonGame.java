cd1a87322b1ead5967de31fe6c40efe13923c410
namkyu0127 <2020136112@g.shingu.ac.kr>
2023-09-20 15:00:01 +0900

    week04 v20

--- week04/src/PokemonGame.java
+++ week04/src/PokemonGame.java
@@ -1,12 +1,25 @@
 
 public class PokemonGame {
     public static void main(String[] args) {
-        NoFly noFly = new NoFly();
-        Pikachu pikachu = new Pikachu(noFly);
-       // pikachu.setFlyable(new NoFly()); // 프로그램 실행 중 동적으로 나는 방식을 변경
+        System.out.println("포켓몬 게임을 시작합니다\n야생 포켓몬이 나타났습니다.");
+   //     System.out.println(Math.random()); //0.0 <= x < 1.0
+   //     System.out.println((int)(Math.random()*6)+1); //1 <= x 6
 
-
-        pikachu.performFly();
+        //적군 포켓몬스터 랜덤 생성
+        int enemyPick = (int)(Math.random()*3);
+        if (enemyPick == 0) {
+            NoFly noFly = new NoFly();
+            Pikachu pikachu = new Pikachu(noFly);
+        } else if (enemyPick == 1) {
+            NoFly noFly = new NoFly();
+            Squirtle squirtle = new Squirtle(noFly);
+        } else if (enemyPick == 2) {
+            Wings wings = new Wings();
+            Charizard charizard = new Charizard(wings);
+        } else {
+            System.out.println("실행 안 됨");
+             //여기는 영원히 실행 안 됩니다.
+        }
     }
 }
 
